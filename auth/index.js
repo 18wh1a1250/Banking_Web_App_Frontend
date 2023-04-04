5921820a5a887d00701cc752912b9ab43177ee61
18wh1a1250 <69299941+18wh1a1250@users.noreply.github.com>
2023-04-04 13:14:56 +0530

    Add files via upload

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,43 @@
+//isLoggedIn=> is logged in. if in local storage we have the token then he is logged in
 
+
+export const isLogggedIn=()=>{
+  let data = localStorage.getItem("data")
+  if(data != null){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+//doLogin=> data we pass will be set to local storage
+
+export const doLogin=(data,next)=>{
+    localStorage.setItem("data",JSON.stringify(data))
+    next()
+};
+//doLogout=> data will be removed from local storage
+
+export const doLogout=(next)=>{
+    localStorage.removeItem("data")
+    next()
+};
+
+// get currentUser
+export const getCurrentUserDetail=()=>{
+    if(isLogggedIn()){
+        return JSON.parse(localStorage.getItem("data")).user;
+    }
+    else{
+        return undefined;
+    }
+};
+
+export const getToken=()=>{
+  if(isLogggedIn()){
+    return JSON.parse(localStorage.getItem("data")).token
+  }else{
+    return null;
+  }
+}
